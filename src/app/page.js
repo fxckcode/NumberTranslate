9bbf5b277c11dfc8b6a7d234feeb9a946bc14b15
fxckcode <diegoale8582@hotmail.com>
2024-06-01 18:00:53 -0500

    responsive

--- src/app/page.js
+++ src/app/page.js
@@ -28,11 +28,11 @@ function HomePage() {
   };
 
   return (
-    <div className='flex flex-col gap-5 p-10 rounded bg-zinc-900 shadow-xl w-1/2 h-1/3'>
+    <div className='flex flex-col gap-5 p-10 rounded bg-zinc-900 shadow-xl lg:w-1/2 lg:h-1/3 w-[80%] h-[60%]'>
       <h1 className='text-center font-bold text-2xl text-white'>NumberTranslate</h1>
-      <form onSubmit={onSubmit} className='flex flex-row gap-5 items-center w-full h-full'>
+      <form onSubmit={onSubmit} className='flex lg:flex-row gap-5 flex-col items-center w-full h-full'>
         <textarea type='text' onInput={handleInput} value={prompt} className='p-3 block bg-neutral-700 text-white w-full h-full rounded-md border border-neutral-600 focus:border-blue-500 focus:ring-blue-500 text-base' placeholder='Enter a number'></textarea>
-        <button className='bg-green-500 p-3 rounded-md block mt-2 text-white h-min text-base font-medium'>
+        <button type='submit' className='bg-green-500 p-3 rounded-md block mt-2 text-white h-min text-base font-medium'>
           Generate
         </button>
         <textarea className='p-3 block bg-neutral-700 text-white w-full rounded-md border border-neutral-600 h-full focus:border-blue-500 focus:ring-blue-500 text-base' readOnly value={result.words && result.words} placeholder='Result'></textarea>
